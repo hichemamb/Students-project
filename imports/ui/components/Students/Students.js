2d6b74ce487c39f5b83bb5297015fa7d37300771
Hichem AMAR BENSABER <amar-bensaber.h@sfeir.com>
2019-01-15 11:04:26 +0100

    [feature] Added mongoDB collection + with form sent data to the back to display them as a list

--- imports/ui/components/Students/Students.js
+++ imports/ui/components/Students/Students.js
@@ -1,47 +1,72 @@
 import React, {Component} from 'react';
+import {listStudents} from '../../../api/list-students';
+import { withTracker } from 'meteor/react-meteor-data';
 
 class Students extends Component {
 
     state = {
-        list: [],
-        firstNameInput:''
+        firstNameInput: '',
+        lastNameInput: '',
+        gitHubInput: '',
     };
 
-
-    handleChange = (event) => {
+    handleChangeFirstname = (event) => {
+        event.preventDefault();
         this.setState({
             firstNameInput: event.target.value
-        })
-
+        });
     };
 
-    handleSubmit = () => {
+    handleChangeLastname = (event) => {
         this.setState({
-            list: [...this.state.list, {"firstName" : this.state.firstNameInput}]
-        })
+            lastNameInput: event.target.value
+        });
+    };
+
+    handleChangeGithub = (event) => {
+        this.setState({
+            gitHubInput: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        //console.log(listStudents.find({}).fetch());
+        listStudents.insert({
+            firstName: this.state.firstNameInput,
+            lastName: this.state.lastNameInput,
+            gitHub: this.state.gitHubInput
+        });
     };
 
 
     render() {
-        const list = this.state.list;
         return (
             <div className="Students">
-
+                <form>
                     <input type="text" value={this.state.firstNameInput}
-                    onChange={this.handleChange}/>
-                    <button onClick={this.handleSubmit}>Valider</button>
+                           onChange={this.handleChangeFirstname}/>
+                    <input type="text" value={this.state.lastNameInput}
+                           onChange={this.handleChangeLastname}/>
+                    <input type="text" value={this.state.gitHubInput}
+                           onChange={this.handleChangeGithub}/>
 
-                <ul>
-                {
-                    list.map((element) =>
-                    <li>{element.firstName}</li>
-                    )
-                }
-                </ul>
+                    <button onClick={this.handleSubmit}>Valider</button>
+                    <ul>
+                        {
+                            this.props.list.map((element, index) =>
+                                <li key={index}>{element.firstName} {element.lastName} {element.gitHub}</li>
+                            )
+                        }
+                    </ul>
+                </form>
             </div>
         );
     }
 }
 
-
-export default Students;
+export default withTracker(() => {
+    return {
+        list: listStudents.find({}).fetch(),
+    };
+})(Students);
