f2bce5d927561b059213c989536dcee1134bfd50
Hichem AMAR BENSABER <amar-bensaber.h@sfeir.com>
2019-01-17 15:09:41 +0100

    [feature] Update 1 chosen element from students list

--- imports/ui/components/Students/Students.js
+++ imports/ui/components/Students/Students.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import {listStudents} from '../../../api/list-students';
-import { withTracker } from 'meteor/react-meteor-data';
+import {withTracker} from 'meteor/react-meteor-data';
 
 class Students extends Component {
 
     state = {
         data: {
+            firstNameInput: '',
+            lastNameInput: '',
+            gitHubInput: '',
+        },
+        modifData: {
             firstNameInput: '',
             lastNameInput: '',
             gitHubInput: '',
@@ -16,6 +21,13 @@ class Students extends Component {
         const data = this.state.data;
         this.setState({
             data: {...data, [content]: event.target.value}
+        });
+    };
+
+    handleChangeUpdate = (content) => event => {
+        const modifData = this.state.modifData;
+        this.setState({
+            modifData: {...modifData, [content]: event.target.value}
         });
     };
 
@@ -33,31 +45,65 @@ class Students extends Component {
         });
     };
 
+    handleUpdate = (event) => {
+        event.preventDefault();
+        listStudents.update({_id: localStorage.getItem('id')}, {
+            $set: {
+                firstName: this.state.modifData.firstNameInput,
+                lastName: this.state.modifData.lastNameInput,
+                gitHub: this.state.modifData.gitHubInput
+            }
+        });
+    };
+
     removeItemByID = (id) => (event) => {
         event.preventDefault();
-        console.log(id);
         listStudents.remove(id);
     };
 
+    updateItem = (event) => {
+        event.preventDefault();
+        const list = this.props.list;
+        localStorage.setItem('id', list[event.target.value]._id);
+    };
+
     render() {
+        const firstNameInput = this.state.data.firstNameInput;
+        const lastNameInput = this.state.data.lastNameInput;
+        const gitHubInput = this.state.data.gitHubInput;
+
         return (
             <div className="Students">
                 <form>
-                    <input type="text" value={this.state.firstNameInput}
+                    <input type="text" value={firstNameInput}
                            onChange={this.handleChange('firstNameInput')}/>
-                    <input type="text" value={this.state.lastNameInput}
+                    <input type="text" value={lastNameInput}
                            onChange={this.handleChange('lastNameInput')}/>
-                    <input type="text" value={this.state.gitHubInput}
+                    <input type="text" value={gitHubInput}
                            onChange={this.handleChange('gitHubInput')}/>
 
                     <button onClick={this.handleSubmit}>Valider</button>
+
                     <ul>
                         {
                             this.props.list.map((element, index) =>
-                                <li key={index}>{element.firstName} {element.lastName} {element.gitHub} <button onClick={this.removeItemByID(element._id)}>Supprimer</button></li>
+                                <li key={index}>{element.firstName} {element.lastName} {element.gitHub}
+                                    <button onClick={this.removeItemByID(element._id)}>Supprimer</button>
+                                    <button value={index} onClick={this.updateItem}>Modifier</button>
+                                </li>
                             )
                         }
                     </ul>
+                </form>
+                <form>
+                    <input type="text"
+                           onChange={this.handleChangeUpdate('firstNameInput')}/>
+                    <input type="text"
+                           onChange={this.handleChangeUpdate('lastNameInput')}/>
+                    <input type="text"
+                           onChange={this.handleChangeUpdate('gitHubInput')}/>
+
+                    <button onClick={this.handleUpdate}>Modifier</button>
                 </form>
             </div>
         );
